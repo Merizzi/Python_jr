86950cd704f4c6d3cbca45d9ef57b0b2102c70d3
Merizzi <pablo.merissi@hotmail.com>
2025-03-07 20:48:36 -0300

    [Py] - Adicionando pequeno desafio

--- teste.py
+++ teste.py
@@ -8,4 +8,4 @@ nome = input("Digite o seu nome:")
 idade = int(input("Digite a sua idade:"))
 peso = float (input("Digite o seu peso :"))
 
-print("O seu nome é:" + nome + ",", "A sua idade é:"+ idade + ",", "O seu peso é:"+ peso)
+print("O seu nome é:" + nome + ",", "A sua idade é:"+ idade + ",", "O seu peso é:" + peso)
