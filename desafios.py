30af59e375ab530c2c67903d7440597ad46203c5
Merizzi <pablo.merissi@hotmail.com>
2025-03-16 20:58:08 -0300

    [Python] - Adicionando alguns exercicios

--- desafios.py
+++ desafios.py
@@ -1,5 +1,41 @@
-for i in range (0, 21, 2):
-    if i % 2  == 0 :
-            print ( i )
-    else:
-        break
+# Desafio a Parte:
+# for i in range (0, 21, 2):
+#     if i % 2  == 0 :
+#             print ( i )
+#     else:
+#         break
+
+# 1. Faça um programa que imprima "Estou aprendendo Python!" na tela. - Feito
+#>> print("Estou aprendendo python!") 
+
+# 2. Faça um programa que peça ao usuário para digitar um número e, em seguida, imprima na tela a mensagem "O número digitado foi <numero>"
+# numero = int(input("Digite o seu número favorito:"))
+# print(f"O seu número favorito é: {numero}")
+
+# 3. Faça um programa que peça ao usuário para digitar seu nome e, em seguida, imprima na tela a mensagem "Olá, <nome>!"
+# nome = input("Digite o seu nome:")
+# print(f"Olá, {nome}, tudo bem com você?")
+
+# 4. Faça um programa que realize a soma e subtração de duas variáveis, sendo uma do tipo inteiro e outra do tipo decimal, 
+# exibindo o valor da soma e da subtração no seguinte formato: "O valor da soma é: <soma>" & "O valor da subtração é: <subtracao>".
+# x = int(input("Digite o seu número inteiro:"))
+# y = float(input("Digite o seu número decimal:"))
+# soma = x + y
+# sub = x - y
+# print(f"O valor da soma é: {soma}"+ f"\nO valor da subtração é: {sub}")
+
+
+# 5. Faça um programa que receba a idade do usuário e retorne a idade dele somada em 10 anos no seguinte formato: Sua idade daqui a 10 anos será: <idade + 10 anos>.
+# 6. Faça um programa que leia algo digitado pelo usuário, mostre seu tipo e tudo a respeito dele (dica: utiliza as funções is).
+# 7. Faça um programa receba algo digitado pelo usuário e verifique se ele digitou alguma coisa, retornando True caso tenha e False caso não tenha.
+# 8. Faça um programa que receba os seguintes dados de um funcionário: nome, idade e salario. Na empresa que esse funcionário trabalha, seu salário é aumentado de ano em ano em R$ 800,90. Sabendo disso, imprima o nome, idade e salário do funcionário daqui a 1 ano no seguinte formato: "O funcionário <nome>, daqui a 1 ano, terá <idade> anos, recebendo um salário igual a R$<salario>.
+# 9. Faça um programa que peça ao usuário para digitar dois números e mostre na tela o resultado da soma, subtração, multiplicação, divisão e resto da divisão desses números.
+# 10. Faça um programa que peça ao usuário o raio de um círculo e exiba na tela a área e o perímetro desse círculo (considere pi = 3.14). Aproxime para 2 casas decimais.
+# 11. Faça um programa que peça ao usuário o preço de um produto e exiba o preço com um desconto de 10%.
+# 12. Faça um programa que leia a temperatura em graus Celsius e exiba a temperatura em graus Fahrenheit. A fórmula para converter de Celsius para Fahrenheit é: F = (9/5)*C + 32.
+# 13. Faça um programa que peça ao usuário para digitar 3 números inteiros e exiba a média aritmética desses números. Aproxime para 1 casa decimal.
+# 14. Faça um programa que leia o peso e a altura de uma pessoa e exiba o índice de massa corporal (IMC) dela. A fórmula para calcular o IMC é: IMC = peso/altura², com aproximação em 3 casas decimais.
+# 15. Faça um programa que leia dois números inteiros do usuário e troque seus valores, ou seja, se o primeiro número for 5 e o segundo número for 7, o programa deve fazer com que o primeiro número seja igual a 7 e o segundo número seja igual a 5.
+# 16. Escreva um programa Python que leia um número inteiro e verifique se ele é par ou ímpar.
+# 17. Escreva um programa Python que leia um número e retorne seu quadrado, sua raíz quadrada e sua raíz cúbica, com aproximação em 2 casas decimais.
+# 18. Escreva um programa Python que leia o valor de dois catetos e retorne o valor da hipotenusa, assumindo que seja possível formar um triângulo.
